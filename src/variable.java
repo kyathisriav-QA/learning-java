8387f4df7d1fee423ff18279050aa8f15627e683
kyathisri-QA <kyathisriav@gmail.com>
2026-01-10 11:34:38 +0530

    variables

---
+++ src/variable.java
@@ -0,0 +1,14 @@
+public class variable{
+    public static void main(String[] args) {
+        ///  datatype variablename = value;
+        int a = 93;   ///local variable
+        char y = 'b';
+        boolean f = true;
+        float e = 3;
+        double c = 7.8888;
+        System.out.println(a);
+        System.out.println(y);
+        System.out.println(f);
+        System.out.println(e);
+    }
+}
