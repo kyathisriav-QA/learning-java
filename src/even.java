9428c1bae11be330b64d3fc21faf3e10355ddc37
kyathisri-QA <kyathisriav@gmail.com>
2026-02-28 10:30:50 +0530

    loop

--- src/even.java
+++ src/even.java
@@ -1,9 +1,7 @@
  class even {
     public static void main(String[] args){
-        for(int i=1;i<=20;i++){
-            if(i%2==0){
+        for(int i=2;i<=20;i=i+2){
               System.out.println(i);
-            }
         }
     }
 }
